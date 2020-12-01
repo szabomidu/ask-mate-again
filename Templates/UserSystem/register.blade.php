fd9062ae52661e739f25161dfef229bba7913279
bodroghykristof <bodroghykristof@gmail.com>
2020-12-01 14:54:35 +0100

    Add inputs on HTML

--- Templates/UserSystem/register.blade.php
+++ Templates/UserSystem/register.blade.php
@@ -1,1 +1,12 @@
 <h1>Register</h1>
+
+<label for="username">Username</label>
+<input type="text" id="username" placeholder="Username" required>
+
+<label for="password_one">Password</label>
+<input type="password" id="password_one" placeholder="Password" required>
+
+<label for="password_two">Password Again</label>
+<input type="password" id="password_two" placeholder="Password again" required>
+
+<button id="submit-button">Register</button>
