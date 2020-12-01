3e95eb978a5586ede9e6d04877163d3d1d1a864d
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-01 21:09:59 +0100

    File formatting

--- Templates/main.blade.php
+++ Templates/main.blade.php
@@ -11,42 +11,40 @@
 	@include("header")
 
 	<div class="welcome-text">
-		<h1 class="header">Welcome to the Gamper Plumber Inc. by Crosswalk Transform</h1>
+		<h1 class="main-text">Welcome to the Gamper Plumber Inc. by Crosswalk Transform</h1>
 		<h2 class="text"> This is the best place for a beginner plumber, <br>
 			here you can find the answers you are looking for and help others with your knowledge!</h2>
 	</div>
 
-@include("navbar")
-<div>
-	<table>
-		<thead>
-		<tr>
-			<th>ID</th>
-			<th>Submission Time</th>
-			<th>Title</th>
-			<th>Message</th>
-			<th>Image</th>
-			<th>Vote Number</th>
-		</tr>
-		</thead>
+	@include("navbar")
 
-		<tbody>
+	<div>
+		<table class="content-table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Submission Time</th>
+					<th>Title</th>
+					<th>Message</th>
+					<th>Image</th>
+					<th>Vote Number</th>
+				</tr>
+			</thead>
 
-		@foreach($questions as $question)
-			<tr>
-				<td>{{$question->get("id")}}</td>
-				{{--				<td>{{ $question->get("submission_time") }}</td>--}}
-				<td>SUBMISSION TIME</td>
-				<td>{{$question->get("title")}}</td>
-				<td>{{$question->get("message")}}</td>
-				<td>IMAGE</td>
-				<td>{{$question->get("vote_number")}}</td>
-			</tr>
-		@endforeach
-
-		</tbody>
-
-	</table>
-</div>
+			<tbody>
+				@foreach($questions as $question)
+				<tr>
+					<td>{{$question->get("id")}}</td>
+					{{--				<td>{{ $question->get("submission_time") }}</td>--}}
+					<td>SUBMISSION TIME</td>
+					<td>{{$question->get("title")}}</td>
+					<td>{{$question->get("message")}}</td>
+					<td>IMAGE</td>
+					<td>{{$question->get("vote_number")}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
