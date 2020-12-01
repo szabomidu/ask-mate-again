49254fb1291aef4fc80e8a7769f5b602e2099164
bodroghykristof <bodroghykristof@gmail.com>
2020-12-01 13:38:57 +0100

    Add DB query

--- App/Controller/MainController.php
+++ App/Controller/MainController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use BK_Framework\Database\Connection\Connection;
+use BK_Framework\Database\QueryTools\Queries;
+
 class MainController extends BaseController
 {
 
@@ -17,6 +20,12 @@ class MainController extends BaseController
 
 	public function run()
 	{
-		echo "Main Page";
+		$pdo = Connection::getConnection(self::$dbConfig);
+		$sql = "SELECT id, title, message, vote_number
+				FROM question
+				ORDER BY id DESC
+				LIMIT 5";
+		$result = Queries::queryAll($pdo, $sql);
+		foreach($result as $record) echo $record->get('title');
 	}
 }
