f80c2120a410be912031fe17851b26f6972ee80b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 13:50:13 +0100

    Removed labels and added class tags

--- Templates/UserSystem/register.blade.php
+++ Templates/UserSystem/register.blade.php
@@ -9,19 +9,18 @@
 </head>
 <body>
 	<div class="form">
-		<h1>Register</h1>
+		<div class="title"><h1>Registration</h1></div>
 
-		<label for="username">Username</label>
-		<input type="text" id="username" placeholder="Username">
+		<div class="content">
+			<input type="text" id="username" placeholder="Username">
 
-		<label for="password_one">Password</label>
-		<input type="password" id="password_one" placeholder="Password">
+			<input type="password" id="password_one" placeholder="Password">
 
-		<label for="password_two">Password Again</label>
-		<input type="password" id="password_two" placeholder="Password again">
+			<input type="password" id="password_two" placeholder="Password again">
+		</div>
 
 		<div id="error-message"></div>
-		<button id="submit-button">Register</button>
+		<div class="submit"><button type="submit" id="submit-button">Register</button></div>
 	</div>
 </body>
 </html>
