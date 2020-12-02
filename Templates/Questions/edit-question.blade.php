77a8581ab077f7eb8407fbe25c592b6a975d7723
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-02 16:20:11 +0100

    Created template for question editing page

--- Templates/Questions/edit-question.blade.php
+++ Templates/Questions/edit-question.blade.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <title>Gamper Plumber Inc.</title>
     <link rel="stylesheet" type="text/css" href="../../static/stylesheets/add.css">
+    <script type="module" src="../../static/js/editQuestion.js" defer></script>
 </head>
 
 <body>
 
 @include("header")
 @include("navbar")
-<h1>Add your question!</h1>
-<form action="/api/edit-question" id="edit-question-form" method="POST">
-    <input type="text" id="title" name="title" value={{$questionData->get("title")}} required><br>
-    <textarea name="message" id="message" cols="30" rows="10" form="add-question-form" required>{{$questionData->get("message")}} </textarea><br>
-    <input type="submit" value="Edit question">
-</form>
+
+<h1>Edit your question!</h1>
+<input type="text" id="title" name="title" value={{$questionData->get("title")}} data-id={{$questionData->get("id")}} required><br>
+<textarea name="message" id="message" cols="30" rows="10" form="add-question-form"
+          required>{{$questionData->get("message")}} </textarea><br>
+<input id="submit-button" type="submit" value="Edit question">
+<div id="error-container"></div>
 </body>
 
 
