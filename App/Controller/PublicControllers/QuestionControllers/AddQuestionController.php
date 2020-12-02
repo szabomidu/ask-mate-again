e3fb5a812ec75d7e63c2473565de79f42aa5ec4c
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 10:14:44 +0100

    Add empty new files for public endpoint

---
+++ App/Controller/PublicControllers/QuestionControllers/AddQuestionController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller\PublicControllers\QuestionControllers;
+
+
+use App\Controller\BaseController;
+
+class AddQuestionController extends BaseController
+{
+
+    public function run()
+    {
+        // TODO: Implement run() method.
+    }
+}
---
+++ App/Routes/QuestionRoutes.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Routes;
+
+
+class QuestionRoutes
+{
+
+}
