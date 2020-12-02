eae082b0d9c92911d16c3ea55111eb86598e2cc8
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 18:41:01 +0100

    Implementing falling items

--- Templates/UserSystem/login.blade.php
+++ Templates/UserSystem/login.blade.php
@@ -5,10 +5,25 @@
     <title>Gamper Plumber Inc.</title>
 
     <link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
+    <link rel="stylesheet" type="text/css" href="../../static/stylesheets/falling.css">
     <script type="module" src="../../static/js/login.js" defer></script>
 
 </head>
 <body>
+    <div class="leaves">
+        <section>
+            <div class="set">
+                <div><img src="../../static/pictures/gardening-1.png"></div>
+                <div><img src="../../static/pictures/gardening-5.png"></div>
+                <div><img src="../../static/pictures/gardening-3.png"></div>
+                <div><img src="../../static/pictures/gardening-4.png"></div>
+                <div><img src="../../static/pictures/gardening-5.png"></div>
+                <div><img src="../../static/pictures/gardening-2.png"></div>
+                <div><img src="../../static/pictures/gardening-3.png"></div>
+                <div><img src="../../static/pictures/gardening-4.png"></div>
+            </div>
+        </section>
+    </div>
     <div class="form">
         <div class="title"><h1>Login</h1></div>
 
