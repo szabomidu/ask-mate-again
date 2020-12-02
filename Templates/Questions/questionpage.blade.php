ce9a413beeb2a726df103a8517a96465bae0ac9b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-03 00:05:35 +0100

    Added edit/delete answer links and style

--- Templates/Questions/questionpage.blade.php
+++ Templates/Questions/questionpage.blade.php
@@ -55,6 +55,8 @@
                 <div class="votes">
                     <p>{{$answer->get("vote_number")}} people likes this answer.</p>
                 </div>
+                <a href="/edit-question?id={{\BK_Framework\SuperGlobal\Get::get("id")}}">Edit answer not working</a>
+                <a href="/delete-question?id={{\BK_Framework\SuperGlobal\Get::get("id")}}">Delete answer not working</a>
             </div>
             <div class="answer-message">
                 <p>{{$answer->get("message")}}</p>
