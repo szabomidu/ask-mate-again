8af1a9c8f7cbcc5ffb5de7980a25ce5c669c0596
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 10:16:07 +0100

    Add new route /add-question

--- App/Routes/QuestionRoutes.php
+++ App/Routes/QuestionRoutes.php
@@ -4,7 +4,18 @@
 namespace App\Routes;
 
 
-class QuestionRoutes
+use App\Controller\PublicControllers\UserSystemControllers\RegistrationController;
+use BK_Framework\Router\Router;
+
+class QuestionRoutes implements RouteInitializer
 {
+
+	function init(): void
+	{
+		Router::add("/add-question", function () {
+			$controller = new RegistrationController("Questions");
+			$controller->run();
+		}, "GET");
+	}
 
 }
