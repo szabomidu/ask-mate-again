355175c8014aeeffc1744d8a612b2d5bdcc824d5
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-02 15:28:39 +0100

    Formatted code and added new endpoint /edit-question

--- App/Routes/QuestionRoutes.php
+++ App/Routes/QuestionRoutes.php
@@ -6,6 +6,7 @@ namespace App\Routes;
 use App\Controller\PublicControllers\QuestionController;
 use App\Controller\APIControllers\QuestionControllers\APIAddQuestionController;
 use App\Controller\PublicControllers\QuestionControllers\AddQuestionController;
+use App\Controller\PublicControllers\QuestionControllers\EditQuestionController;
 use BK_Framework\Router\Router;
 
 class QuestionRoutes implements RouteInitializer
@@ -17,15 +18,21 @@ class QuestionRoutes implements RouteInitializer
             $controller = new QuestionController($id);
             $controller->run();
         }, "GET");
-    
-  	Router::add("/add-question", function () {
-			$controller = new AddQuestionController();
-			$controller->run();
-		}, "GET");
 
-		Router::add("/api/add-question", function () {
-			$controller = new APIAddQuestionController();
-			$controller->run();
-		}, "POST");
-	}
+        Router::add("/add-question", function () {
+            $controller = new AddQuestionController();
+            $controller->run();
+        }, "GET");
+
+        Router::add("/api/add-question", function () {
+            $controller = new APIAddQuestionController();
+            $controller->run();
+        }, "POST");
+
+        Router::add("/edit-question", function () {
+            $id = $_GET["id"];
+            $controller = new EditQuestionController($id);
+            $controller->run();
+        }, "GET");
+    }
 }
