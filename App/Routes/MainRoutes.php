9a5d8c3289d34288b5968ae7cd49e1e5190fdad8
Szkini22 <tamas.catanzaro98@gmail.com>
2020-12-02 15:04:46 +0100

    /all-tags endpoint added to main routes

--- App/Routes/MainRoutes.php
+++ App/Routes/MainRoutes.php
@@ -5,6 +5,7 @@ namespace App\Routes;
 
 
 use App\Controller\PublicControllers\AllQuestionController;
+use App\Controller\PublicControllers\AllTagsController;
 use App\Controller\PublicControllers\MainController;
 use BK_Framework\Router\Router;
 
@@ -20,6 +21,11 @@ class MainRoutes implements RouteInitializer
 
         Router::add("/all", function () {
             $controller = new AllQuestionController();
+            $controller->run();
+        }, "GET");
+
+        Router::add("/all-tags", function () {
+            $controller = new AllTagsController();
             $controller->run();
         }, "GET");
     }
