90e02ca9c1d4907a9c555191d020a133c7a4ffb3
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 12:04:15 +0100

    Remove Jinja clutter from navbar.blade.php

--- Templates/navbar.blade.php
+++ Templates/navbar.blade.php
@@ -12,13 +12,3 @@
 		@endif
 	</ul>
 </div>
-
-
-{{--	<ul>--}}
-{{--		<li><a style="text-decoration: none" href="{{ url_for('users') }}">Users</a></li>--}}
-{{--		<li><a style="text-decoration: none" href="{{ url_for('user_page', user_id=user_id) }}">My profile</a></li>--}}
-{{--		<li><a style="text-decoration: none" href="{{ url_for('registration') }}">Registration</a></li>--}}
-{{--		<li><a style="text-decoration: none" href="{{ url_for('login') }}">Login</a></li>--}}
-{{--		<li><a style="text-decoration: none" href="{{ url_for('tags', source="welcome_page") }}">Tags</a></li>--}}
-{{--		<li style="text-align: center"><a href="{{ url_for('display_list') }}">Show all questions</a></li>--}}
-{{--	</ul>--}}
