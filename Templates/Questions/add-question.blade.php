9d7b438d9526b641d0e14e67341efb1cced4ce0c
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 10:26:04 +0100

    Create basic HTML form

--- Templates/Questions/add-question.blade.php
+++ Templates/Questions/add-question.blade.php
@@ -1,1 +1,10 @@
 <h1>Add Question</h1>
+
+<form action="/api/add-question" id="add-question-form" method="POST">
+	<label for="title">Question title</label>
+	<input type="text" id="title" name="title">
+	<label for="message">Question Body</label>
+	<textarea name="message" id="message" cols="30" rows="10" form="add-question-form"></textarea>
+	<input type="submit" value="Ask">
+</form>
+
