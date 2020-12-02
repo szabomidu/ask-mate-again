bb0b0a9a8ffb0ad6e1ff7d7f799e79393d488cdf
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 14:21:57 +0100

    Display tags related to question on question page

--- Templates/questionpage.blade.php
+++ Templates/questionpage.blade.php
@@ -11,6 +11,14 @@
 @include("header")
 @include("navbar")
 
+<div id="tag-container">
+	@foreach($tags as $tag)
+		<div class="tag-card">
+			<p>{{$tag->get("name")}}</p>
+		</div>
+	@endforeach
+</div>
+
 <div>
     <p>{{$questionData->get("title")}}</p>
     <p>{{$questionData->get("message")}}</p>
