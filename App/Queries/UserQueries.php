f66035e3e4cdb3031f1a0c873d47c32e31106e31
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 13:29:03 +0100

    Fix issues with ResultSet constructor and checkIfUsernameValid method

--- App/Queries/UserQueries.php
+++ App/Queries/UserQueries.php
@@ -12,43 +12,46 @@ use function Symfony\Component\String\s;
 class UserQueries
 {
 
-	public static function checkIfUsernameExists(PDO $pdo, User $user) : bool
+	public static function checkIfUsernameExists(PDO $pdo, User $user): bool
 	{
 		$sql = "SELECT EXISTS
 					(SELECT *
 					FROM registered_user
 					WHERE email = :email)
     			AS 'exists'";
-		return Queries::queryOne($pdo, $sql, ["email"=>$user->getUsername()])->get("exists");
+		return Queries::queryOne($pdo, $sql, ["email" => $user->getUsername()])->get("exists");
 	}
 
-	public static function registerNewUser(PDO $pdo, User $user) : int
+	public static function registerNewUser(PDO $pdo, User $user): int
 	{
 		$sql = "INSERT INTO registered_user
 				(email, password_hash)
 				VALUES (:email, :password)";
 		$hashedPassword = password_hash($user->getPassword(), PASSWORD_DEFAULT);
-		return Queries::executeAndReturnWithId($pdo, $sql, ["email"=>$user->getUsername(),
-															"password"=>$hashedPassword]);
+		return Queries::executeAndReturnWithId($pdo, $sql, ["email" => $user->getUsername(),
+			"password" => $hashedPassword]);
 	}
 
-    public static function checkIfUsernameValid(PDO $pdo, string $username, $password) : bool
-    {
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "SELECT EXISTS 
-                    (SELECT email 
-                    FROM registered_user)
-                    WHERE email = :email, password_hash = :hashedPassword)
-                    AS 'valid'";
-        return Queries::queryOne($pdo, $sql, ["email"=>$username, "password_hashed"=>$hashedPassword])->get("valid");
-    }
+	public static function checkIfUsernameValid(PDO $pdo, string $username, $password): bool
+	{
+		$sql = "SELECT password_hash
+				FROM registered_user
+				WHERE email = :email";
+		$result = Queries::queryOne($pdo, $sql, ["email" => $username]);
 
-    public static function getUserIdByUsername(PDO $pdo, string $username)
-    {
-        $sql = "SELECT id
+		if ($result === null) return false;
+		else {
+			$storedHash = ($result->get("password_hash"));
+			return password_verify($password, $storedHash);
+		}
+	}
+
+	public static function getUserIdByUsername(PDO $pdo, string $username)
+	{
+		$sql = "SELECT id
                 FROM registered_user
                 WHERE email = :username";
-        return Queries::queryOne($pdo, $sql, ["email"=>$username])->get("id");
-    }
+		return Queries::queryOne($pdo, $sql, ["email" => $username])->get("id");
+	}
 
 }
