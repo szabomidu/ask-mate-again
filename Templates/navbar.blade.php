8eef4b7f43d8744ad53e36abe5cc6565e667685c
Szkini22 <tamas.catanzaro98@gmail.com>
2020-12-02 15:05:08 +0100

    navbar extended with a show all tags link

--- Templates/navbar.blade.php
+++ Templates/navbar.blade.php
@@ -15,8 +15,10 @@
         @if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
             <li><a href="/tags">Tags</a></li>
             <li><a href="/add-question">Add question</a></li>
+            <li><a href="/all-tags">Show all tags</a></li>
         @endif
 
         	<li><a href="/all">Show all questions</a></li>
+
     </ul>
 </div>
