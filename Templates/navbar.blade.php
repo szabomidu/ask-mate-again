62eeee983316d4385b3b3bc7390dc09c61ecb5f2
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 13:56:35 +0100

    Navbar working properly

--- Templates/navbar.blade.php
+++ Templates/navbar.blade.php
@@ -1,21 +1,22 @@
 <!--suppress ALL -->
 <div class="navbar">
-	<ul>
+    <ul>
 
-		@if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
-			<li><a href="/register">Users</a></li>
-			<li><a href="/register">My Profile</a></li>
-		@endif
+        @if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
+            <li><a href="/register">Users</a></li>
+            <li><a href="/register">My Profile</a></li>
+        @endif
 
-		<li><a href="/login">Login</a></li>
+        @if (!\BK_Framework\SuperGlobal\Session::isLoggedIn())
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Registration</a></li>
+        @endif
 
-		@if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
-			<li><a href="/tags">Tags</a></li>
-		@endif
-    @if(\BK_Framework\SuperGlobal\Session::isLoggedIn())
-			<li><a href="/add-question">Add question</a></li>
-		@endif
+        @if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
+            <li><a href="/tags">Tags</a></li>
+            <li><a href="/add-question">Add question</a></li>
+        @endif
 
-		<li><a href="/all">Show all questions</a></li>
-	</ul>
+        	<li><a href="/all">Show all questions</a></li>
+    </ul>
 </div>
