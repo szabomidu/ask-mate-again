542e278d0b7c1389bf513fb967f1b032a80d6e67
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-02 14:10:43 +0100

    Added getAnswerToQuestionById method

--- App/Queries/QuestionQueries.php
+++ App/Queries/QuestionQueries.php
@@ -11,40 +11,50 @@ use PDO;
 class QuestionQueries
 {
 
-	public static function getFiveMostRecentQuestions(PDO $pdo) : array
-	{
-		$sql = "SELECT id, title, message, vote_number
+    public static function getFiveMostRecentQuestions(PDO $pdo): array
+    {
+        $sql = "SELECT id, title, message, vote_number
 				FROM question
 				ORDER BY id DESC
 				LIMIT 5";
-		return Queries::queryAll($pdo, $sql);
-	}
+        return Queries::queryAll($pdo, $sql);
+    }
 
     public static function getQuestionDataById(PDO $pdo, int $id)
     {
         $sql = "SELECT id, title, message
                 FROM question
                 WHERE id = :id";
-        return Queries::queryOne($pdo, $sql, ["id"=>$id]);
+        return Queries::queryOne($pdo, $sql, ["id" => $id]);
     }
 
 
-	public static function add(PDO $pdo, Question $question, string $userId) : void
-	{
-		$sql = "INSERT INTO question (id_registered_user, title, message, vote_number)
+    public static function add(PDO $pdo, Question $question, string $userId): void
+    {
+        $sql = "INSERT INTO question (id_registered_user, title, message, vote_number)
 				VALUES (:userId, :title, :message, 0)";
 
-		Queries::execute($pdo, $sql, ["userId"=>$userId,
-									"title"=>$question->getTitle(),
-									"message"=>$question->getMessage()]);
-	}
+        Queries::execute($pdo, $sql, ["userId" => $userId,
+            "title" => $question->getTitle(),
+            "message" => $question->getMessage()]);
+    }
 
-    public static function getAllQuestions(PDO $pdo) : array
+    public static function getAllQuestions(PDO $pdo): array
     {
         $sql = "SELECT id, title, message, vote_number
 				FROM question
 				ORDER BY id DESC";
         return Queries::queryAll($pdo, $sql);
+    }
+
+    public static function getAnswersToQuestionById(PDO $pdo, int $id)
+    {
+	    $sql = "SELECT message,
+                    submission_time,
+                    vote_number
+                FROM answer
+                WHERE id_question = :id";
+        return Queries::queryAll($pdo, $sql, ["id_question" => $id]);
     }
 
 }
