7223ac348cfd97302fabb5b5ee093daab857173e
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 16:45:23 +0100

    Removed some links which are not necessary

--- Templates/navbar.blade.php
+++ Templates/navbar.blade.php
@@ -2,19 +2,14 @@
 <div class="navbar">
     <ul>
 
-        @if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
-            <li><a href="/register">Users</a></li>
-            <li><a href="/register">My Profile</a></li>
-        @endif
-
         @if (!\BK_Framework\SuperGlobal\Session::isLoggedIn())
             <li><a href="/login">Login</a></li>
             <li><a href="/register">Registration</a></li>
         @endif
 
         @if (\BK_Framework\SuperGlobal\Session::isLoggedIn())
+            <li><a href="/users">Users</a></li>
             <li><a href="/tags">Tags</a></li>
-            <li><a href="/addtag">Add tag</a></li>
             <li><a href="/add-question">Add question</a></li>
         @endif
 
