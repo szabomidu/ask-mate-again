63fac1bfda7dc302fb8d41ee6ade78a59f331af9
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 10:21:59 +0100

    Set up login page

---
+++ Templates/UserSystem/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gamper Plumber Inc.</title>
+    <script type="module" src="../../static/js/login.js" defer></script>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <label for="username">Username</label>
+    <input type="text" id="username" placeholder="Username">
+
+    <label for="password">Password</label>
+    <input type="password" id="password" placeholder="Password">
+
+    <div id="error-message"></div>
+
+    <button id="submit-button">Register</button>
+</body>
+</html>
