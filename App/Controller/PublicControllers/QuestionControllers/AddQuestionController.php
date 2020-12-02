70af3c4a20791825b9b83001acf7fa85531faef4
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 12:10:42 +0100

    Redirect to main page if not logged in

--- App/Controller/PublicControllers/QuestionControllers/AddQuestionController.php
+++ App/Controller/PublicControllers/QuestionControllers/AddQuestionController.php
@@ -5,6 +5,8 @@ namespace App\Controller\PublicControllers\QuestionControllers;
 
 
 use App\Controller\BaseController;
+use BK_Framework\SuperGlobal\Server;
+use BK_Framework\SuperGlobal\Session;
 
 class AddQuestionController extends BaseController
 {
@@ -12,6 +14,7 @@ class AddQuestionController extends BaseController
     public function run()
     {
     	session_start();
+    	if (!Session::isLoggedIn()) Server::redirect("/");
 		$this->view("Questions.add-question");
     }
 
