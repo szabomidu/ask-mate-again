0fbd4734a83bb948ce9eb15768323d813e8ad426
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 10:47:37 +0100

    Linked stylesheet and added div container

--- Templates/UserSystem/login.blade.php
+++ Templates/UserSystem/login.blade.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>Gamper Plumber Inc.</title>
+
+    <link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
     <script type="module" src="../../static/js/login.js" defer></script>
+
 </head>
 <body>
-    <h1>Login</h1>
+    <div class="form">
+        <h1>Login</h1>
 
-    <label for="username">Username</label>
-    <input type="text" id="username" placeholder="Username">
+        <label for="username">Username</label>
+        <input type="text" id="username" placeholder="Username">
 
-    <label for="password">Password</label>
-    <input type="password" id="password" placeholder="Password">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Password">
 
-    <div id="error-message"></div>
+        <div id="error-message"></div>
 
-    <button id="submit-button">Register</button>
+        <button id="submit-button">Register</button>
+    </div>
+
 </body>
 </html>
