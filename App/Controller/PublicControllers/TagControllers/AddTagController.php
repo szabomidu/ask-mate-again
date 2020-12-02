f9b2ba16898c351ef3d9e35d8c95e68eaa2dbf33
Szkini22 <tamas.catanzaro98@gmail.com>
2020-12-02 15:43:39 +0100

    AddTagController.php run method extended with query

--- App/Controller/PublicControllers/TagControllers/AddTagController.php
+++ App/Controller/PublicControllers/TagControllers/AddTagController.php
@@ -6,12 +6,18 @@ namespace App\Controller\PublicControllers\TagControllers;
 
 
 use App\Controller\BaseController;
+use App\Queries\TagQueries;
+use BK_Framework\Database\Connection\Connection;
 
 class AddTagController extends BaseController
 {
 
     public function run()
     {
-        $this->view("Tags.add-tag");
+        session_start();
+        $pdo = Connection::getConnection(self::$dbConfig);
+        $tags = TagQueries::getAllTags($pdo);
+        $this->view("Tags.add-tag", ["tags"=>$tags]);
     }
 }
+
