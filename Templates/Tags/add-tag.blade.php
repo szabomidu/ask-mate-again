f5913b513f4d7c443455afae23e3d9b6929d548f
bodroghykristof <bodroghykristof@gmail.com>
2020-12-02 15:29:26 +0100

    Add basic HTML for add-tag endpoint

--- Templates/Tags/add-tag.blade.php
+++ Templates/Tags/add-tag.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Gamper Plumber Inc.</title>
-
-    <link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
-
+	<meta charset="UTF-8">
+	<title>Gamper Plumber Inc.</title>
+	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
 </head>
 <body>
 <div class="form">
-    <div class="title"><h1>Add tag</h1></div>
+	<div class="title"><h1>Add tag</h1></div>
 
-    <div class="content">
-        <input type="text" id="username" placeholder="Tag name">
-    </div>
+	<select name="tags" id="tags">
+		@foreach($tags as $tag)
+			<option value="{{$tag->get("name")}}"></option>
+		@endforeach
+	</select>
 
-    <div class="submit"><button type="submit" id="submit-button">Add</button></div>
+	<input type="text" placeholder="Custom Tag">
+	<div class="submit">
+		<button type="submit" id="submit-button">Add</button>
+	</div>
 
 </div>
-
 </body>
 </html>
