5830c8c6b6f53fbb2c5b184b242f38a24dea2a08
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-02 10:47:15 +0100

    Linked stylesheet and added div container

--- Templates/UserSystem/register.blade.php
+++ Templates/UserSystem/register.blade.php
@@ -3,21 +3,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Gamper Plumber Inc.</title>
+
+	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
 	<script type="module" src="../../static/js/registration.js" defer></script>
 </head>
 <body>
-	<h1>Register</h1>
+	<div class="form">
+		<h1>Register</h1>
 
-	<label for="username">Username</label>
-	<input type="text" id="username" placeholder="Username">
+		<label for="username">Username</label>
+		<input type="text" id="username" placeholder="Username">
 
-	<label for="password_one">Password</label>
-	<input type="password" id="password_one" placeholder="Password">
+		<label for="password_one">Password</label>
+		<input type="password" id="password_one" placeholder="Password">
 
-	<label for="password_two">Password Again</label>
-	<input type="password" id="password_two" placeholder="Password again">
+		<label for="password_two">Password Again</label>
+		<input type="password" id="password_two" placeholder="Password again">
 
-	<div id="error-message"></div>
-	<button id="submit-button">Register</button>
+		<div id="error-message"></div>
+		<button id="submit-button">Register</button>
+	</div>
 </body>
 </html>
