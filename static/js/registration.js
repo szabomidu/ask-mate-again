42dd17693293d5923b5333f4f384e552ad3f25db
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-02 10:35:13 +0100

    Reformatted code

--- static/js/registration.js
+++ static/js/registration.js
@@ -3,46 +3,37 @@ import {dataHandler} from "./datahandler.js";
 init();
 
 function init() {
-	document.querySelector("#submit-button").addEventListener("click", validateUserData)
+    document.querySelector("#submit-button").addEventListener("click", validateUserData)
 }
 
 function validateUserData() {
-	const username = document.querySelector("#username").value;
-	const passwordOne = document.querySelector("#password_one").value;
-	const passwordTwo = document.querySelector("#password_two").value;
-	const errorContainer = document.querySelector("#error-message");
+    const username = document.querySelector("#username").value;
+    const passwordOne = document.querySelector("#password_one").value;
+    const passwordTwo = document.querySelector("#password_two").value;
+    const errorContainer = document.querySelector("#error-message");
 
-	if (checkNonEmptyFields(username,  passwordOne, passwordTwo, errorContainer)) {
-
-		if (checkEqualPasswords(passwordOne, passwordTwo, errorContainer)) {
-			const user = {username: username, passwordOne: passwordOne, passwordTwo: passwordTwo};
-			dataHandler._api_post("/api/register", user, redirectToMainPage, displayErrorMessages);
-		}
-
-	}
-
+    if (checkNonEmptyFields(username, passwordOne, passwordTwo, errorContainer)) {
+        if (checkEqualPasswords(passwordOne, passwordTwo, errorContainer)) {
+            const user = {username: username, passwordOne: passwordOne, passwordTwo: passwordTwo};
+            dataHandler._api_post("/api/register", user, redirectToMainPage, displayErrorMessages);
+        }
+    }
 }
 
 function checkNonEmptyFields(userInput, passwordOne, passwordTwo, errorContainer) {
-
-	if (userInput === "" || passwordOne === "" || passwordTwo === "") {
-		errorContainer.innerHTML = "Please fill out all fields!";
-		return false;
-	}
-
-	return true;
-
+    if (userInput === "" || passwordOne === "" || passwordTwo === "") {
+        errorContainer.innerHTML = "Please fill out all fields!";
+        return false;
+    }
+    return true;
 }
 
 function checkEqualPasswords(passwordOne, passwordTwo, errorContainer) {
-
-	if (passwordOne !== passwordTwo) {
-		errorContainer.innerHTML = "Mismatched passwords!";
-		return false;
-	}
-
-	return true;
-
+    if (passwordOne !== passwordTwo) {
+        errorContainer.innerHTML = "Mismatched passwords!";
+        return false;
+    }
+    return true;
 }
 
 function redirectToMainPage(data) {
@@ -58,7 +49,5 @@ function redirectToMainPage(data) {
 }
 
 function displayErrorMessages() {
-
-	console.log("Response not JSON")
-
+    console.log("Response not JSON")
 }
