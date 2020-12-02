da672633c117cae37b89d8322004e7fc74a30bd4
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-02 17:31:40 +0100

    Created new class RemoveTagController.php

---
+++ App/Controller/APIControllers/TagControllers/RemoveTagController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller\APIControllers\TagControllers;
+
+
+use App\Controller\BaseController;
+
+class RemoveTagController extends BaseController
+{
+
+    public function run()
+    {
+        // TODO: Implement run() method.
+    }
+}
