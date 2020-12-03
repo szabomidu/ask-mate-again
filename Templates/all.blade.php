211368b8affd358189eb4f1fe286ac64978f1251
Szkini22 <tamas.catanzaro98@gmail.com>
2020-12-03 10:11:15 +0100

    all.blade.php page extended by submission time

--- Templates/all.blade.php
+++ Templates/all.blade.php
@@ -36,7 +36,7 @@
             <tr>
                 <td>{{$question->get("id")}}</td>
                 {{--				<td>{{ $question->get("submission_time") }}</td>--}}
-                <td>SUBMISSION TIME</td>
+                <td>{{ $question->get("submission_time") }}</td>
                 <td><a href="/question?id={{$question->get("id")}}">{{$question->get("title")}}</a></td>
                 <td>{{$question->get("message")}}</td>
                 <td>IMAGE</td>
