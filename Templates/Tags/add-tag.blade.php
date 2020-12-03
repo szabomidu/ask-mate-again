6d147f8944d09a11e87fac378194a51e8f470ee4
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-03 10:48:03 +0100

    Styled addTag page, removed lines from loginRegister that are not used there

--- Templates/Tags/add-tag.blade.php
+++ Templates/Tags/add-tag.blade.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Gamper Plumber Inc.</title>
-	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/loginRegister.css">
+	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/addTag.css">
 	<script type="module" src="../../static/js/add-tag.js" defer></script>
 </head>
 <body>
 <div class="form">
 	<div class="title"><h1>Add tag</h1></div>
 
-	<button id="tag-input-button">I would like to add new tag</button>
-	<button id="tag-select-button">I would like to select a tag</button>
+	<button id="tag-input-button">Add new tag</button>
+	<button id="tag-select-button">Select from existing tags</button>
 
 	<select name="tags" id="tags" class="hide-both">
 		@foreach($tags as $tag)
