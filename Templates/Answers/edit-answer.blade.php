1adaa70f2aadb1dd7284cfd5fdb0beabec5c7504
bodroghykristof <bodroghykristof@gmail.com>
2020-12-03 14:08:39 +0100

    Attempt to style edit answer page

--- Templates/Answers/edit-answer.blade.php
+++ Templates/Answers/edit-answer.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Gamper Plumber Inc.</title>
-    <link rel="stylesheet" type="text/css" href="../../static/stylesheets/add.css">
-    <script type="module" src="../../static/js/editAnswer.js" defer></script>
+	<meta charset="UTF-8">
+	<title>Gamper Plumber Inc.</title>
+	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/add.css">
+	<link rel="stylesheet" type="text/css" href="../../static/stylesheets/main.css">
+	<script type="module" src="../../static/js/editAnswer.js" defer></script>
 </head>
 
 <body>
@@ -12,9 +13,12 @@
 @include("header")
 @include("navbar")
 
-<h1 id="title" data-id={{$answerData->get("id")}}  data-questionid={{$questionId}}>Edit your answer!</h1>
-<textarea name="message" id="message" cols="30" rows="10" form="add-question-form">{{$answerData->get("message")}}</textarea><br>
-<input id="submit-button" type="submit" value="Edit answer">
-<div id="error-container"></div>
+<div class="form">
+	<h1 id="title" data-id={{$answerData->get("id")}}  data-questionid={{$questionId}}>Edit your answer!</h1>
+	<textarea name="message" id="message" cols="30" rows="10"
+			  form="add-question-form">{{$answerData->get("message")}}</textarea><br>
+	<input id="submit-button" type="submit" value="Edit answer">
+	<div id="error-container"></div>
+</div>
 </body>
 </html>
